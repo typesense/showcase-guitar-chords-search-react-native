8b209f20f9ab67082b98986ede92568aa7bd933b
Phi Hung Hoang <hphihung795@gmail.com>
2024-02-26 20:04:53 +0700

    scripts to start and index data into typesense server

--- src/utils/typesense.ts
+++ src/utils/typesense.ts
@@ -1,13 +1,20 @@
 import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter';
+import Constants from 'expo-constants';
+
+const {
+  expoConfig: { hostUri },
+} = Constants;
+
+const localhost = hostUri.split(`:`).shift();
 
 export const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
   server: {
-    apiKey: process.env.EXPO_PUBLIC_TYPESENSE_SEARCH_ONLY_API_KEY,
+    apiKey: process.env.EXPO_PUBLIC_TYPESENSE_SEARCH_ONLY_API_KEY || 'xyz',
     nodes: [
       {
-        host: process.env.EXPO_PUBLIC_TYPESENSE_HOST,
-        port: parseInt(process.env.EXPO_PUBLIC_TYPESENSE_PORT),
-        protocol: process.env.EXPO_PUBLIC_TYPESENSE_PROTOCOL,
+        host: process.env.EXPO_PUBLIC_TYPESENSE_HOST || localhost,
+        port: parseInt(process.env.EXPO_PUBLIC_TYPESENSE_PORT || '8108'),
+        protocol: process.env.EXPO_PUBLIC_TYPESENSE_PROTOCOL || 'http',
       },
     ],
   },
